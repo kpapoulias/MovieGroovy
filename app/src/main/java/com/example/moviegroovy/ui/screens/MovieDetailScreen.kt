9bbdb27be663e8f2a07c73fc845f415f99f3e9da
Kostis Papoulias <papoulias.kostis@gmail.com>
2025-03-12 13:13:26 +0200

    Improve Movie Details Screen

--- app/src/main/java/com/example/moviegroovy/ui/screens/MovieDetailScreen.kt
+++ app/src/main/java/com/example/moviegroovy/ui/screens/MovieDetailScreen.kt
@@ -3,12 +3,16 @@ package com.example.moviegroovy.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -19,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -42,34 +47,55 @@ fun MovieDetailScreen(
             .verticalScroll(rememberScrollState())
             .padding(16.dp)
     ) {
-        Image(
-            painter = rememberAsyncImagePainter(
-                model = movie.imageUrl,
-                error = painterResource(R.drawable.image_24px)
-            ),
-            contentDescription = "Movie Poster",
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(300.dp),
-            contentScale = ContentScale.Fit
-        )
+                .clip(RoundedCornerShape(20.dp)),
+
+            ) {
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = movie.imageUrl,
+                    error = painterResource(R.drawable.image_24px)
+                ),
+                contentDescription = "Movie Poster",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(600.dp)
+                    .clip(RoundedCornerShape(20.dp)),
+            )
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
             text = movie.title,
             fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.titleMedium
         )
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = "⭐ ${movie.rating}",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Medium,
-            color = MaterialTheme.colorScheme.secondary
-        )
+        Row {
+            Text(
+                text = "⭐ ${movie.rating}",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.secondary
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "(${movie.voteCount})",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.secondary
+            )
+        }
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
